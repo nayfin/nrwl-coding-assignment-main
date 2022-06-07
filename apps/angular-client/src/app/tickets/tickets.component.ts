285e0282f8af423d08011eda01d5c6302e8f8b47
Nathan <natjkern@gmail.com>
2022-06-06 19:47:55 -0500

    enables creating new ticket, maps user names to tickets

--- apps/angular-client/src/app/tickets/tickets.component.ts
+++ apps/angular-client/src/app/tickets/tickets.component.ts
@@ -1,24 +1,44 @@
-import { Ticket } from '@acme/shared-models';
-import { Component, OnInit } from '@angular/core';
+import { Ticket, User } from '@acme/shared-models';
+import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 import { ControlType, FormConfig } from '@tft/crispr-forms';
 import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { ApiService } from '../api.service';
 
 type StatusOptions = 'complete' | 'incomplete'
+type TicketUI = Ticket & { assigneeName: string | null};
 
 @Component({
   selector: 'acme-tickets',
   templateUrl: './tickets.component.html',
-  styleUrls: ['./tickets.component.css'],
+  styleUrls: ['./tickets.component.scss'],
 })
 export class TicketsComponent {
-  tickets$ = this.api.tickets();
+  newTicketsSubject = new BehaviorSubject<Ticket[]>([]);
+  // This is a funky way of doing this, normally this would be handled by updating a store
+
   users$ = this.api.users();
+
+  tickets$: Observable<TicketUI[]> = combineLatest([
+    this.api.tickets(),
+    this.newTicketsSubject,
+    this.users$
+  ]).pipe(
+    // map assignee name to ticket to make it easier to grab from the template
+    map(([apiTickets, newTickets, users ]) => [...apiTickets, ...newTickets].map(ticket => {
+      const assigneeName = ticket.assigneeId ? this.getNameForUserId(ticket.assigneeId, users) : null
+      return {
+        ...ticket,
+        assigneeName
+      }
+    }))
+  );
+
   filterSubject = new BehaviorSubject<StatusOptions[]>([]);
 
-  filteredTickets$: Observable<Ticket[]> = combineLatest([
+  filteredTickets$: Observable<TicketUI[]> = combineLatest([
     this.tickets$,
-    this.filterSubject
+    this.filterSubject,
   ]).pipe(
     map(([unfilteredTickets, filter]) => {
       // return all tickets if not filtering
@@ -30,7 +50,7 @@ export class TicketsComponent {
       })
     })
   )
-
+  // the configuration for the filter form
   filterConfig: FormConfig = {
     fields: [
       {
@@ -51,9 +71,48 @@ export class TicketsComponent {
       }
     ]
   }
-  constructor(private api: ApiService) {}
+  // the configuration for the new ticket form
+  newTicketConfig: FormConfig = {
+    fields: [
+      {
+        controlType: ControlType.HEADING,
+        label: 'New Ticket'
+      },
+      {
+        controlType: ControlType.INPUT,
+        controlName: 'description',
+        validators: [Validators.required],
+        label: 'Descriptions'
+      },
+      {
+        controlType: ControlType.BUTTON,
+        label: 'CREATE TICKET'
+      }
+    ]
+  };
+  // Used to hide the new ticket form when submitting
+  submitting = false;
+
+  constructor(
+    private api: ApiService
+  ) {}
 
   updateFilter(event: {statusFilter: StatusOptions[]}) {
     this.filterSubject.next(event.statusFilter || [])
+  }
+
+  createNewTicket(ticketForm: FormGroup) {
+    if(ticketForm.valid) {
+      const ticketValue: {description: string} = ticketForm.value;
+      this.submitting = true;
+      this.api.newTicket(ticketValue).subscribe((res) => {
+        this.newTicketsSubject.next([...this.newTicketsSubject.value, res])
+        this.submitting = false;
+      });
+    }
+  }
+
+  getNameForUserId(userId: number, users: User[]) {
+    return users.find(user => user.id === userId)?.name || null
   }
 }
